cdddd1ccc1d1fb1b635a900c8b0cd478cb8ef280
MArbaz872 <101636839+MArbaz872@users.noreply.github.com>
2024-10-29 12:22:02 +0500

    Update db.js

--- src/Config/db.js
+++ src/Config/db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config(); // Ensure this is loaded
 
 const connection = mysql.createConnection({
-  host: 'najempay.net', // Use IP instead of domain name
+  host: '82.112.255.73', // Use IP instead of domain name
   port: 3306, // MySQL default port
   user: 'root',
   password: 'Admin12345$$',
